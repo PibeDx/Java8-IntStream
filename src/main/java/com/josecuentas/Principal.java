2fb71e109878b2c75bcd561b008a3e205e659d74
PibeDx <jcuentast@gmail.com>
2017-02-26 14:44:54 -0500

    commit initial

---
+++ src/main/java/com/josecuentas/Principal.java
@@ -0,0 +1,15 @@
+package com.josecuentas;
+
+import java.util.stream.IntStream;
+
+/**
+ * Created by iPibeDx on 26/02/17.
+ * Base: http://www.arquitecturajava.com/trabajando-con-java-8-intstream/?utm_campaign=shareaholic&utm_medium=twitter&utm_source=socialnetwork
+ */
+
+
+public class Principal {
+    public static void main(String[] args) {
+        IntStream it = IntStream.of(1,2,3,4,5);
+    }
+}
